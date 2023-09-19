2f2cac5711a3dfa29e5074e3eac55c51083ad6f4
@lmdlalana023 <innomdlalana@gmail.com>
2023-09-19 14:12:21 +0200

    Restyle home page

--- network/admin.py
+++ network/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post,PostLike
+from .models import Post,PostLike,User
 
+admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(PostLike)
--- network/models.py
+++ network/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    pass 
 
 class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
